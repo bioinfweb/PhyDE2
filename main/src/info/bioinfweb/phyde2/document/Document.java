38d4c8c4521e7b9be1a6176ee44254d336dff5dd
JonasBohn <bohn@bioinfweb.info>
2017-10-20 08:05:21 +0000

    Built PhyDE 2 project and created packages document and gui. Created subpackages in gui package for actions. Import commons, jphyloio, libralign and tic packages. Wrote License information (built NOTIVE.txt, GLP.txt, APACHE-LICENDE.txt).

---
+++ main/src/info/bioinfweb/phyde2/document/Document.java
@@ -0,0 +1,25 @@
+/*
+ * PhyDE 2 - An alignment editor for phylogenetic purposes
+ * Copyright (C) 2017  Ben Stöver, Jonas Bohn, Kai Müller
+ * <http://bioinfweb.info/PhyDE2>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.phyde2.document;
+
+
+
+public class Document {
+  //TODO Implement. (Add properties for an alignment model and an UndoManager.)
+}
