ffa695ecbcf1ca44360894f642efb73691bbde3d
SarahvanGroen <SarahvanGroen@bioinfweb.info>
2018-11-15 14:08:34 +0000

    commons.swing dependency updated. Version information updated.

--- main/src/info/bioinfweb/phyde2/Main.java
+++ main/src/info/bioinfweb/phyde2/Main.java
@@ -51,7 +51,7 @@ public class Main extends ProgramMainClass {
 	 * Launch the application.
 	 */
 	public Main(){
-		super(new ApplicationVersion(0, 0, 0, 33, ApplicationType.ALPHA));
+		super(new ApplicationVersion(0, 1, 0, 48, ApplicationType.ALPHA));
 	}
 	
 	
